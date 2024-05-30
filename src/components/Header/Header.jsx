201b71d9cf18447caec72bc1d63f1f82c577cb06
minerchan <samuelcavalcantimg@gmail.com>
2024-05-30 04:15:05 -0300

    iniciando na tela de 400px de largura

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,8 @@ import logo from '../../asset/logo.png';
 function Header() {
   return (
     <nav className='Header'>
-        <h1>Beers On Tap</h1>
-        <img src={logo} alt='logo' />
-        <div className='flexABV'>
-            <h3 className='ABV'>ABV</h3>
-            <p>Alcohol by Volume</p>
-        </div>
+        <h1 className='BeersOnTap'>Beers On Tap</h1>
+        <img className='logo' src={logo} alt='logo' />
         <div className='flexTamanhos'>
             <h2>Tamanhos</h2>
             <p>300ml / 500ml</p>
