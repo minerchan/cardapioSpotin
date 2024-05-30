425d0f6d82f13c41339b5de13b2b91ac858593fe
minerchan <samuelcavalcantimg@gmail.com>
2024-05-30 04:24:43 -0300

    1.1

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,12 @@
 import './Card.scss';
 
-function Card({imagem, nomeDaCerveja, }) {
+function Card({imagem, Cerveja, Marca, preco}) {
   return (
     <section className='Card'>
-        <img src={imagem} alt={nomeDaCerveja}/>
+        <img src={imagem} alt={Cerveja}/>
+        <h1>{Marca}</h1>
+        <h3>{Cerveja}</h3>
+        <h2>{preco}</h2>
     </section>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
+import Card from '../../components/Card/Card';
 import Header from '../../components/Header/Header';
 import './Home.scss';
+import tarin from '../../asset/tarin.png';
+import hankzbeer from '../../asset/hankzbeer.png';
 
 function Home() {
   return (
     <>
       <Header/>
+      <Card imagem={tarin}/>
+      <Card imagem={hankzbeer}/>
+      <Card imagem={hankzbeer}/>
     </>
   )
 }
