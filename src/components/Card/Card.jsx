4b9cd0e3b4428a34c1f471eb30bb60d5694bbc9b
minerchan <samuelcavalcantimg@gmail.com>
2024-05-30 04:53:14 -0300

    primeira versao pronta

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,9 +4,11 @@ function Card({imagem, Cerveja, Marca, preco}) {
   return (
     <section className='Card'>
         <img src={imagem} alt={Cerveja}/>
-        <h1>{Marca}</h1>
-        <h3>{Cerveja}</h3>
-        <h2>{preco}</h2>
+        <div className='flexMarcaCerveja'>
+            <h1>{Marca}</h1>
+            <h3>{Cerveja}</h3>
+        </div>
+        <h2>{`R$ ${preco}`}</h2>
     </section>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,19 @@ import Header from '../../components/Header/Header';
 import './Home.scss';
 import tarin from '../../asset/tarin.png';
 import hankzbeer from '../../asset/hankzbeer.png';
+import xeque from '../../asset/xeque.png';
 
 function Home() {
   return (
     <>
       <Header/>
-      <Card imagem={tarin}/>
-      <Card imagem={hankzbeer}/>
-      <Card imagem={hankzbeer}/>
+      <Card Marca={'Tarin'} Cerveja={"American IPA"} imagem={tarin} preco={"24 / 34"}/>
+      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"stout"} preco={"26 / 38"}/>
+      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"SOUR FRUIT"} preco={"18 / 30"}/>
+      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"pilsen"} preco={"14 / 18"}/>
+      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"munich dunkel"} preco={"16 / 20"}/>
+      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"session ipa"} preco={"16 / 20"}/>
+      <Card Marca={'Xeque Mate'} imagem={xeque} preco={"15"}/>
     </>
   )
 }
