a92cdabf1ff29a977fc286e452bce549a191735e
minerchan <samuelcavalcantimg@gmail.com>
2024-05-30 05:48:45 -0300

    versão com drop down pronta

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,36 @@
 import './Card.scss';
+import arrowUp from '../../asset/arrowUp.png';
+import { useState } from 'react';
 
-function Card({imagem, Cerveja, Marca, preco}) {
+function Card({imagem, Cerveja, Marca, preco, descricao}) {
+    const [tamanho, setTamanho] = useState("20rem")
+    const [rotate, setRotate] = useState("0deg")
+
+    function mudanca(){
+        if(tamanho === "20rem"){
+            setTamanho("fit-content")
+            setRotate("90deg")
+        }
+        else{
+            setTamanho("20rem")
+            setRotate("0deg")
+            
+        }
+    }
   return (
-    <section className='Card'>
-        <img src={imagem} alt={Cerveja}/>
-        <div className='flexMarcaCerveja'>
-            <h1>{Marca}</h1>
-            <h3>{Cerveja}</h3>
-        </div>
-        <h2>{`R$ ${preco}`}</h2>
-    </section>
+    <div className='caixaDeDescricao' style={{height: tamanho}}>
+        <section className='Card'>
+            <img src={imagem} alt={Cerveja}/>
+            <div className='flexMarcaCerveja'>
+                <h1>{Marca}</h1>
+                <h3>{Cerveja}</h3>
+            </div>
+            
+            <h2>{`R$ ${preco}`}</h2>
+            <button onClick={()=> mudanca()}><img style={{rotate: rotate}} src={arrowUp} alt="Saber mais" /></button>
+        </section>
+        <p className='descricao'>{descricao}</p>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,13 +9,13 @@ function Home() {
   return (
     <>
       <Header/>
-      <Card Marca={'Tarin'} Cerveja={"American IPA"} imagem={tarin} preco={"24 / 34"}/>
-      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"stout"} preco={"26 / 38"}/>
-      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"SOUR FRUIT"} preco={"18 / 30"}/>
-      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"pilsen"} preco={"14 / 18"}/>
-      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"munich dunkel"} preco={"16 / 20"}/>
-      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"session ipa"} preco={"16 / 20"}/>
-      <Card Marca={'Xeque Mate'} imagem={xeque} preco={"15"}/>
+      <Card Marca={'Tarin'} Cerveja={"American IPA"} imagem={tarin} preco={"24 / 34"} descricao={"Triple IPA com adições pesadas de lúpulos que trazem notas de frutas maduras, dentre eles Nelson Sauvin no formato Hop Hash. O rótulo rende homenagem ao Marco Zero na linda cidade de Recife."}/>
+      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"stout"} preco={"26 / 38"} descricao={"Achocolatado de Gente Grande é o novo lançamento dos nossos parceiros. Esta cerveja é um verdadeiro licor de chocolate amargo com quantidades absurdas de nibs de cacau. Possui aroma de sorvete, achocolato amargo e ao leite e um incrível toffee."}/>
+      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"SOUR FRUIT"} preco={"18 / 30"} descricao={"Sour com adições de maracuja, goiaba e amora. Extremamente refrescante e aromatica."}/>
+      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"pilsen"} preco={"14 / 18"} />
+      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"munich dunkel"} preco={"16 / 20"} descricao={"Munich Dunkel é um estilo de cerveja tradicional de Munique, Alemanha. Com carga expressiva de malte base Munich e outros especiais, ela é de cor cobre avermelhada a marrom escuro profundo."}/>
+      <Card Marca={'Hankzbier'} imagem={hankzbeer} Cerveja={"session ipa"} preco={"16 / 20"} descricao={"Session IPA extremamente refrescante, perfeita para dias de calor."}/>
+      <Card Marca={'Xeque Mate'} imagem={xeque} preco={"15"} descricao={"Uma mistura saborosa de mate, rum, guaraná e limão"}/>
     </>
   )
 }
